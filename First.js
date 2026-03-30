e20fbae8d2424a43eb272684c1c57b147ad5ed59
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-03-30 13:00:09 +0530

    First commit of node.js

---
+++ First.js
@@ -0,0 +1,8 @@
+console.log("Ashish is the best");
+
+const fs = require('fs');
+
+fs.writeFile("output.txt", "Writing File",(err)=>{
+  if(err) console.log("Error occured");
+  else console.log('file written successfully')
+})
